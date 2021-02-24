3ede5d7141fc499bad4e1c7fd17df0b6b9db339c
omara1 <74941531+omara1@users.noreply.github.com>
2021-02-24 07:52:46 +0200

    Update index.js

--- index.js
+++ index.js
@@ -23,10 +23,10 @@ var secondName = prompt("enter the name of second player ");
 
 
 if (randomNumber1 > randomNumber2) {
-  document.querySelector("h1").innerHTML = "🚩 " + fristName + " is a winner";
+  document.querySelector("h1").innerHTML = "🚩 " + fristName + " is a winner!";
 }
 else if (randomNumber2 > randomNumber1) {
-  document.querySelector("h1").innerHTML =  secondName + " is a winner ! 🚩";
+  document.querySelector("h1").innerHTML =  secondName + " is a winner! 🚩";
 }
 else {
   document.querySelector("h1").innerHTML = "Draw!";
