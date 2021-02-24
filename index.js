f5c4d0c50b109db1eeca478347918f1eb5c2d862
omara1 <74941531+omara1@users.noreply.github.com>
2021-02-24 07:35:16 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 
+
 var randomNumber1 = Math.floor(Math.random() * 6) + 1; //1-6
 
 var randomDiceImage = "dice" + randomNumber1 + ".png"; //dice1.png - dice6.png
@@ -17,13 +18,18 @@ var randomImageSource2 = "dice" + randomNumber2 + ".png";
 document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
 
 
-//If player 1 wins
+var fristName = prompt("enter the name of frist player ");
+var secondName = prompt("enter the name of second player ");
+
+
 if (randomNumber1 > randomNumber2) {
-  document.querySelector("h1").innerHTML = "🚩 Play 1 Wins!";
+  document.querySelector("h1").innerHTML = "🚩 " + fristName + "is a winner";
 }
 else if (randomNumber2 > randomNumber1) {
-  document.querySelector("h1").innerHTML = "Player 2 Wins! 🚩";
+  document.querySelector("h1").innerHTML =  secondName + " is a winner ! 🚩";
 }
 else {
   document.querySelector("h1").innerHTML = "Draw!";
+}
+
 }
